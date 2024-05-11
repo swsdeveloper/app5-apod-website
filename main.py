9bf0d97d8590e3cffa0c990f78a5cccfa8cc2d34
Steven Shatz <sshatzdeveloper@gmail.com>
2024-05-11 18:59:34 -0400

    Fixed video handling and added date to web page

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import streamlit as st
 import requests
-from website import display_as_webpage
+from datetime import datetime
+from os import path
+from website import display_webpage
+
+st.set_page_config(layout='wide')
 
 # See: https://api.nasa.gov/ and https://github.com/nasa/apod-api
 
@@ -15,16 +20,26 @@ url = "https://api.nasa.gov/planetary/apod" \
 response = requests.get(url)
 apod_dict = response.json()
 
-# print(apod_dict)
-# for key, value in apod_dict.items():
-#     print(f"{key}: {value}")
-
 title = apod_dict["title"]
 description = apod_dict["explanation"]
+
+media_type = apod_dict["media_type"]
 
 if "hdurl" in apod_dict.keys():
     image_url = apod_dict["hdurl"]  # only for media_type = 'image'
 else:
     image_url = apod_dict["url"]    # for media_type 'image' and 'video'
 
-display_as_webpage(title, description, image_url)
+# Download the image to the "image files" folder.
+# Name it yyyy-mm-dd-{url filename} (using current date)
+if media_type == "image":
+    today = datetime.today().strftime('%Y-%m-%d')
+    image_path = f"image files/{today}-{path.basename(image_url)}"
+    image_response = requests.get(image_url)
+    image = image_response.content
+    with open(image_path, 'wb') as file:
+        file.write(image)
+else:  # "video"
+    image_path = image_url
+
+display_webpage(title, description, image_path, media_type)
--- website.py
+++ website.py
@@ -1,26 +1,29 @@
 import streamlit as st
 from datetime import datetime
-from os import path
 
 
-def display_as_webpage(title: str, description: str, image_url: str) -> None:
+def display_webpage(title: str, description: str, image_path: str, media_type: str) -> None:
     """
     Display the passed info on a streamlit webpage
 
     :param title: title of the image
     :param description: a description of the image
-    :param image_url: url of an image or video
+    :param image_path: path to a downloaded image or url of a video
+    :param media_type: either 'image' or 'video'
     :return:
     """
-    st.set_page_config(layout='wide')
+    page_date = ":blue[" + datetime.today().strftime('%b %d, %Y') + "]"
+    st.header(page_date)
+
+    page_title = "NASA's Astronomy Image of the Day"
+    st.title(page_title)
 
     st.header(title)
 
-    # Download the image to the "image files" folder.
-    # Name it yyyy-mm-dd-{url filename} (using current date)
-    today = datetime.today().strftime('%Y-%m-%d')
-    image_path = f"image files/{today}-{path.basename(image_url)}"
-    st.image(image_path)
+    if media_type == "image":
+        st.image(image_path)  # should point to a file
+    else:
+        st.video(image_path)  # should be a url
 
     st.write(description)
 
