da880994ad4b0c89ab99c6d280cc717f75bdbcdb
Steven Shatz <sshatzdeveloper@gmail.com>
2024-05-11 19:18:16 -0400

    Omit "image files" directory from image path

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ else:
 # Name it yyyy-mm-dd-{url filename} (using current date)
 if media_type == "image":
     today = datetime.today().strftime('%Y-%m-%d')
-    image_path = f"./image files/{today}-{path.basename(image_url)}"
+    # image_path = f"image files/{today}-{path.basename(image_url)}"  # for local testing
+    image_path = f"{today}-{path.basename(image_url)}"  # for deployment to streamlit cloud
     response2 = requests.get(image_url)
     image = response2.content
     with open(image_path, 'wb') as file:
